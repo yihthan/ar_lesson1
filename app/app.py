5a3ce2d6c7f90fa380852fe85fc89e1a95381a69
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2022-12-02 09:24:47 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,34 +10,8 @@ app = Flask(__name__)
 def hello_world():
     request_type_str = request.method
     if request_type_str == 'GET':
-        return render_template('index.html', href2='static/none.png', href3='')
+        return render_template('index.html')
     else:
-        myage = request.form['age']
-        mygender = request.form['gender']
-        mybread = ''
-        if str(myage) =='' or str(mygender) =='':
-            return render_template('index.html', href2='static/none.png', href3='Please insert your age and gender.')
-        else:
-            model = load('app/bread-recommender.joblib')
-            np_arr = np.array([myage, mygender])
-            predictions = model.predict([np_arr])  
-            predictions_to_str = str(predictions)
-            
-            if 'donut' in predictions_to_str:
-                mybread = 'static/donut.png'
-            elif 'croissant' in predictions_to_str:
-                mybread = 'static/croissant.png'
-            elif 'whole grain' in predictions_to_str:
-                mybread = 'static/wholegrain.png'
-            elif 'wheat bread' in predictions_to_str:
-                mybread = 'static/wheatbread.png'
-            elif 'swiss roll' in predictions_to_str:
-                mybread = 'static/swissroll.png'
-            elif 'sandwich' in predictions_to_str:
-                mybread = 'static/sandwich.png'
-            else:
-                mybread = 'static/none.png' 
-                
-            return render_template('index.html', href2=str(mybread), href3='This is the recommendation! (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
+        return render_template('index.html')
         
 
